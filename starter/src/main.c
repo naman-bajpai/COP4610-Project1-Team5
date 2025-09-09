e6e00a9af7249e56128a7dcb5e7593fb2b10971c
bobromero <rromero123102@gmail.com>
2025-09-09 14:59:44 -0400

    added part 2

--- starter/src/main.c
+++ starter/src/main.c
@@ -1,13 +1,15 @@
+#define _POSIX_C_SOURCE 200809L
 #include "lexer.h"
 #include "prompt.h"
 #include <stdio.h>
+#include <string.h>
 int main(void) {
     while (1) {
         printf("%s>",get_prompt());
         fflush(stdout);
 
         char *input = get_input();
-        if (!input) break;        /* allocation failure */
+        if (!input) break;        
         if (input[0] == '\0' && feof(stdin)) { /* EOF */
             free(input);
             break;
@@ -17,7 +19,19 @@ int main(void) {
 
         tokenlist *tokens = get_tokens(input);
         for (int i = 0; i < (int)tokens->size; i++) {
-            printf("token %d: (%s)\n", i, tokens->items[i]);
+            if (tokens->items[i][0]== '$'){
+                const char *env_val = getenv(tokens->items[i] + 1);
+                if (env_val){
+                    free(tokens->items[i]);
+                    tokens->items[i] = strdup(env_val);
+                }
+                else {
+                    free(tokens->items[i]);
+                    tokens->items[i] = strdup(""); 
+                }
+                printf("token %d: (%s)\n", i, tokens->items[i]);
+
+            }
         }
 
         free(input);
